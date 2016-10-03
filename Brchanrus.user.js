535c6fdb9c313fe10db74b712d313ed2bee96d70
Isset <isseq@gameufa.ru>
2016-10-03 14:02:08 +0500

    Test bump version, autoupdate

--- Brchanrus.user.js
+++ Brchanrus.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name            Brchan Rusifikator
-// @version         3
+// @version         3.1
 // @namespace       https://brchan.org/*
 // @author          Y0ba, Isset, pngcrypt
 // @updateURL       https://raw.github.com/Isseq/Brchanrus.user.js/master/Brchanrus.meta.js
