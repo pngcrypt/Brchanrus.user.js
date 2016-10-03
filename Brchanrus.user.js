444288622e63ea8df3a84e0ec0c30190729f6dbd
Isset <isseq@gameufa.ru>
2016-10-03 13:05:47 +0500

    auto update

--- Brchanrus.user.js
+++ Brchanrus.user.js
@@ -3,6 +3,7 @@
 // @version         3
 // @namespace       https://brchan.org/*
 // @author          Y0ba, Isset, pngcrypt
+// @updateURL       https://raw.github.com/Isseq/Brchanrus.user.js/master/Brchanrus.meta.js
 // @run-at          document-start
 // @grant           none
 // @include         https://brchan.org/*
