49a7f46f8a1f835db4e417025d4e5e376ca7070c
Isset <isseq@gameufa.ru>
2016-10-07 01:28:30 +0500

    mod.php?/brus/res/1.html#10

--- Brchanrus.user.js
+++ Brchanrus.user.js
@@ -293,7 +293,7 @@ var cfg = [
 
 
 	// Любая доска / тред под модеркой
-	[/^mod\.php\?\/\w+(|\/|\/.+\.html)$/, [
+	[/^mod\.php\?\/\w+(|\/|\/.+\.html(\#.+)?)$/, [
 		// кнопки модерирования
 		['reg', 'span.controls', [
 			['Spoiler em tudo', 'Спрятать превью изображений'],
@@ -1134,7 +1134,7 @@ document.onreadystatechange = function () {
 
 			// перевод сообщений
 			wf.alert = window.alert;
-			window.alert = function(msg, do_confirm, confirm_ok_action, confirm_cancel_action) 
+			window.alert = function(msg, do_confirm, confirm_ok_action, confirm_cancel_action)
 			{
 				msg = {text: msg};
 				for(let r of posting_replacers) {
