accf723c910aff94db7f6307a8b799f8ab9fa8d8
pngcrypt <png.crypt@yandex.ru>
2016-11-13 19:00:12 +0300

    version up

--- Brchanrus.meta.js
+++ Brchanrus.meta.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name            Brchan Rusifikator
-// @version         3.4.0
+// @version         3.4.1
 // @namespace       https://brchan.org/*
 // @author          Y0ba, Isset, pngcrypt
 // @updateURL       https://raw.github.com/Isseq/Brchanrus.user.js/master/Brchanrus.meta.js
